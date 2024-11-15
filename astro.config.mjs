e584c36f4a28a3502e0793f238dc1ca27a743c14
LolzTheDev <lolzthedev@gmail.com>
2024-11-15 01:35:03 -0600

    base proxy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import { defineConfig } from "astro/config";
 import { ChemicalVitePlugin as Chemical } from "chemicaljs";
+import { server as wisp } from "@mercuryworkshop/wisp-js/server";
 
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   vite: {
     plugins: [
+      {
+        name: "vite-wisp-server",
+        configureServer(server) {
+          server.httpServer?.on("upgrade", (req, socket, head) =>
+            req.url?.startsWith("/f")
+              ? wisp.routeRequest(req, socket, head)
+              : undefined,
+          );
+        },
+      },
       Chemical({
         default: "uv",
         uv: true,
@@ -14,5 +25,6 @@ export default defineConfig({
       }),
     ],
   },
+  output: "server",
   integrations: [tailwind()],
 });
